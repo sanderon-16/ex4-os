0a431f873311561a8f7739a4717bdc6d26e81296
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-07-22 20:00:18 +0300

    made a skeleton

---
+++ VirtualMemory.cpp
@@ -0,0 +1,19 @@
+#include "VirtualMemory.h"
+#include "PhysicalMemory.h"
+
+
+void VMInitialize()
+{
+
+}
+
+int VMread(uint64_t virtualAddress, word_t* value)
+{
+
+}
+
+
+int VMwrite(uint64_t virtualAddress, word_t value)
+{
+
+}
