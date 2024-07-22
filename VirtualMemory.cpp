638d9e7aa7bb6819e511957ed6f349b1807763a9
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-07-22 23:04:50 +0300

    made VMInitialize

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,18 +2,19 @@
 #include "PhysicalMemory.h"
 
 
-void VMInitialize()
-{
+void VMInitialize() {
+    // writes 0 on root
+    word_t zero = 0;
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        PMwrite(0 + i, zero);
+    }
+}
+
+int VMread(uint64_t virtualAddress, word_t *value) {
 
 }
 
-int VMread(uint64_t virtualAddress, word_t* value)
-{
 
-}
-
-
-int VMwrite(uint64_t virtualAddress, word_t value)
-{
+int VMwrite(uint64_t virtualAddress, word_t value) {
 
 }
