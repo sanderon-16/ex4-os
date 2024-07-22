d3b6ee3f9ead8bf308730c6c11c8a915f16f074a
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-07-22 23:28:26 +0300

    made a skelton for get_address_by_priority

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,7 +1,12 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
-int get_Paddress(uint64_t address);
+typedef uint64_t paddress;
+
+paddress get_Paddress(uint64_t address);
+paddress grab_frame(paddress physical_address);
+int get_priority_of_frame(paddress frame);
+
 
 void VMInitialize() {
     // writes 0 on root
@@ -22,7 +27,46 @@ int VMwrite(uint64_t virtualAddress, word_t value)
 }
 
 
-int get_Paddress(uint64_t address) {
+paddress get_Paddress(uint64_t address) {
     int prev_padd = 0;
     return 0;
+}
+
+paddress grab_frame(paddress skip_this_frame){
+    int best_priority = 4;
+    paddress chosen_frame = 0;
+    for (int frame = 0; frame < NUM_FRAMES; ++frame) {
+        if (frame == skip_this_frame)
+        {
+            continue;
+        }
+        // calculate priority:
+        int priority = get_priority_of_frame(frame);
+        if (priority < best_priority){
+            best_priority = priority;
+            chosen_frame = frame;
+        }
+    }
+    // TODO this:
+    switch (best_priority) {
+        case 1:
+            // remove the reference to this frame from its parent.
+            break;
+        case 2:
+            // an unused frame
+            break;
+        case 3:
+            // all frames are already in use,
+            break;
+        default:
+            // exception
+            break;
+    }
+    return 0
+}
+
+int get_priority_of_frame(paddress frame)
+{
+    // TODO this:
+    return 1;
 }
